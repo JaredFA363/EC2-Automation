324db3a5310f4fe3b1943f35c2032cdf32eaf3a0
Jared <jaredawiti2017@gmail.com>
2025-10-12 14:58:38 +0100

    Fix terraform formatting

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,9 +1,9 @@
 output "instance_public_ip" {
-    value = aws_instance.web.public_ip
-    description = "Public IP of the EC2 instance"
+  value       = aws_instance.web.public_ip
+  description = "Public IP of the EC2 instance"
 }
 
 output "healthcheck_url" {
-    value = "http://${aws_instance.web.public_ip}/health"
-    description = "/health endpoint URL"
+  value       = "http://${aws_instance.web.public_ip}/health"
+  description = "/health endpoint URL"
 }
